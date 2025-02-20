e1cf33d6b54d5b168ce23c3bd4e7df8f9ad7cd19
Masoumeh <vahedi.m1993@gmail.com>
2025-02-20 14:45:27 +0100

    Add ollama summarization text task

--- Text Summarization/main.py
+++ Text Summarization/main.py
@@ -1,8 +1,14 @@
 import os
 from WebsiteSummarizationAPI import connectionAPIKey, Website, messagesFor
+from WebsiteSummarizationOllama import callOllamaPackage, callOllamaOpenAI, callOllamaRequest, callOllamaDeepSeek
+
 
 
 def test(url):
+    #----------------------------------
+    #       Testing OpenAI
+    # ---------------------------------
+
     # Initialize and constants
     MODEL, openai = connectionAPIKey()
 
@@ -16,9 +22,43 @@ def test(url):
     print(web.title)
     print(web.text)
 
+    # ----------------------------------
+    #         Testing Ollama
+    # ---------------------------------
+
+    messages = [
+        {"role": "user", "content": "Describe some of the business applications of Generative AI"}
+    ]
+
+    # Method 1: Using requests
+    result_requests = callOllamaRequest(messages)
+    if result_requests:
+        print("Response via requests:")
+        print(result_requests)
+
+    # Method 2: Using the ollama package
+    result_package = callOllamaPackage(messages)
+    print("\nResponse via the ollama package:")
+    print(result_package)
+
+    # Method 3: Using the OpenAI client library
+    result_openai = callOllamaOpenAI(messages)
+    print("\nResponse via the OpenAI client library:")
+    print(result_openai)
+
+    # Method 4: Using the Deepseek R1
+    prompt = (
+        "Please give definitions of some core concepts behind LLMs: "
+        "a neural network, attention and the transformer"
+    )
+    result_deepseek = callOllamaDeepSeek(prompt)
+    print("\nResponse via the Deepseek R1:")
+    print(result_deepseek)
 
 
-def summarize(url):
+
+
+def summarizeWithAPI(url):
     # Initialize and constants
     MODEL, openai = connectionAPIKey()
 
@@ -31,14 +71,36 @@ def summarize(url):
 
 
 
+def summarizeWithOllama(url):
+    website = Website(url)
+    # Using the OpenAI client library
+    result = callOllamaOpenAI(messages=messagesFor(website))
+    return result
+
+
+
+
 def displaySummary(url):
-    summary = summarize(url)
-    print(summary)
+    # Summarization using OpenAI
+    print("\nSummary via OpenAI:")
+    summary_api = summarizeWithAPI(url)
+    print(summary_api)
+
+    # Summarization using Ollama
+    summary_ollama = summarizeWithOllama(url)
+    print("\nSummary via the ollama:")
+    print(summary_ollama)
+
+
+
+
+def main():
+    url = "https://cnn.com"
+    # test(url)
+    displaySummary(url)
 
 
 
 
 if __name__ == "__main__":
-    url = "https://cnn.com"
-    test(url)
-    displaySummary(url)
+    main()
